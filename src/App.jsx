2a64636e0d65942ca6dc6be4e6f215a221ac97fd
EmmanuelU <emmanuelurias60@icloud.com>
2022-12-23 15:26:27 -0800

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
 
 function App() {
 
-  let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+  let [colors, setColors] = useState (['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
 
-  let colorMap = colors.map((color, i) => {
-    return (
-      < ColorBlock color={color} />
-    )
-  })
+  const addColor = (newColor) => {
+    setColors([colors, newColor])
+  }
 
   return (
-    <div className="App">
-        {colorMap}
-    </div>
+<div className="App">
+    {colors.map((color, i) => 
+        <ColorBlock key={i} color={color} />
+    )}
+    <ColorForm addColor={addColor}/>
+</div>
+
   );
 }
 
